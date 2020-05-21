c6a059875b44de5a3c146c108bd4cedc0bcce142
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-05-21 13:58:05 +0200

    README.md with more examples, and with code that can run
    Signed-off-by: Thierry Parmentelat <thierry.parmentelat@inria.fr>

---
+++ podman/containers/__init__.py
@@ -0,0 +1,81 @@
+"""containers provides the operations against containers for a Podman service."""
+import json
+import logging
+
+import podman.errors as errors
+
+
+def list_containers(api, all=None):
+    """List all containers for a Podman service."""
+    path = "/containers/json"
+    query = {}
+    if all is not None:
+        query['all'] = all
+
+    if query:
+        path = api.join(path, query)
+    else:
+        path = api.join(path)
+
+    response = api.request("GET", path)
+    # observed to return None when no containers
+    return json.loads(response.read()) or []
+
+
+def inspect(api, name):
+    """Report on named container for a Podman service.
+       Name may also be a container ID.
+    """
+    try:
+        response = api.request(
+            "GET", api.join("/containers/{}/json".format(api.quote(name)))
+        )
+        return json.loads(response.read())
+    except errors.NotFoundError as e:
+        _report_not_found(e, e.response)
+
+
+def kill(api, name, signal=None):
+    """kill named/identified container"""
+    path = "/containers/{}/kill".format(api.quote(name))
+    if signal is not None:
+        path = api.join(path, {"signal": signal})
+    else:
+        path = api.join(path)
+
+    try:
+        response = api.request("POST", path)
+        response.read() # returns an empty bytes object
+        # return json.loads(response.read())
+        return True
+    except errors.NotFoundError as e:
+        _report_not_found(e, e.response)
+
+
+def remove(api, name, force=None, delete_volumes=None):
+    """Delete container"""
+    path = "/containers/{}".format(api.quote(name))
+    query = {}
+    if force is not None:
+        query['force'] = force
+    if delete_volumes is not None:
+        query['v'] = delete_volumes
+    if query:
+        path = api.join(path, query)
+    else:
+        path = api.join(path)
+
+    try:
+        response = api.request("DELETE", path)
+        response.read() # returns an empty bytes object
+        # return json.loads(response.read())
+        return True
+    except errors.NotFoundError as e:
+        _report_not_found(e, e.response)
+    # xxx need to handle error 409 Conflict error in operation ?
+
+
+def _report_not_found(e, response):
+    body = json.loads(response.read())
+    logging.info(body["cause"])
+    raise errors.ContainerNotFound(body["message"]) from e
